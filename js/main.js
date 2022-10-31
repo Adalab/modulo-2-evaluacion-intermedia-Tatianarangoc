ad82d27d480396a8c0159d76e5b6dfd134ebfdca
Tatiana Arango Cardona <tatianarangoc@gmail.com>
2022-10-31 11:09:12 +0100

    added msj to show points

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ const select_element = document.querySelector('.select');
 const btn_element = document.querySelector('.js_btn');
 const result_element = document.querySelector('.container_text');
 const mensajePc_element = document.querySelector('.mensajepc');
+const puntos_element = document.querySelector('.puntos');
+
+let puntosJugador = 0;
+let puntosPc = 0;
 
 function validarGanador(razaJugador, pc) {
   if (razaJugador > pc) {
+    puntosJugador = puntosJugador + 1;
     return 'Ha Ganado el ejercito del bien! En horabuena.';
   } else if (razaJugador < pc) {
+    puntosPc = puntosPc + 1;
     return ' Ha ganado el ejercito del mal';
   } else {
     return 'Empate';
@@ -27,18 +33,21 @@ function razaPcMensaje(raza) {
   }
 }
 function getRandomNumber(min, max) {
-  return Math.round(Math.random() * (max - min + 1) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 function handleClick(event) {
   event.preventDefault(event);
   const razaJugador = select_element.value;
   const razaPc = getRandomNumber(1, 5);
-  console.log(razaPc);
   const mensajePc = razaPcMensaje(razaPc);
-  console.log(mensajePc);
   const mensaje = validarGanador(razaJugador, razaPc);
   mensajePc_element.innerHTML = 'El pc ha seleccionado: ' + mensajePc;
   result_element.innerHTML = mensaje;
+  puntos_element.innerHTML =
+    'Los puntos del jugador son: ' +
+    puntosJugador +
+    ' y los puntos del pc son:' +
+    puntosPc;
 }
 
 btn_element.addEventListener('click', handleClick);
