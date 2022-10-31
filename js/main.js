8d3f58d44f52e09b3b032498ae05e2b0cbe4f19a
Tatiana Arango Cardona <tatianarangoc@gmail.com>
2022-10-31 10:20:58 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -2,31 +2,25 @@ const select_element = document.querySelector('.select');
 const btn_element = document.querySelector('.js_btn');
 const result_element = document.querySelector('.result');
 
-btn_element.addEventListener('click', (event) => {
-  event.preventDefault();
-});
-
-function razaJugador() {
-  const selectRaza = select_element.value;
-  if (selectRaza === raza1) {
-    ('Peloso con fuerza 1');
-  } else if (selectRaza === raza2) {
-    (' Sureños buenos con fuerza 2');
-  } else if (selectRaza === raza3) {
-    ('Enanos con fuerza 3');
-  } else if (selectRaza === raza4) {
-    ('Númenóreanos con fuerza (4)3');
-  } else if (selectRaza === raza5) {
-    ('Elfos con fuerza 5');
+function validarGanador(razaJugador, pc) {
+  if (razaJugador > pc) {
+    return 'Ha Ganado el ejercito del bien! En horabuena.';
+  } else if (razaJugador < pc) {
+    return ' Ha ganado el ejercito del mal';
+  } else {
+    return 'Empate';
   }
 }
-
-function validarGanador(razaJugador, pc) {}
 
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min + 1) + min);
 }
-const razasMalvadas = () => {
-  const razasMalvadas = getRandomNumber(1, 6);
-  const malvados = razasMalvadas;
-};
+function handleClick(event) {
+  event.preventDefault(event);
+  const razaJugador = select_element.value;
+  const razaPc = getRandomNumber(1, 6);
+  const mensaje = validarGanador(razaJugador, razaPc);
+  result_element.innerHTML = mensaje;
+}
+
+btn_element.addEventListener('click', handleClick);
